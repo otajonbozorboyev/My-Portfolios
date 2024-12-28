31706293a46ad75a09815e8b41a13e8787b85919
Your Correct Name <otajonbozorboyev571@gmail.com>
2024-12-28 09:28:32 +0500

    next update

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index),
     path('about/', AboutView.as_view(), name='about'),
+    path('credentials/', CredentialsView.as_view(), name='credentials'),
+    path('works/', WorksView.as_view(), name='works'),
+    path('works/<slug:slug>/', WorkDetailView.as_view(), name='work_detail'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,7 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from portfolio.models import AboutMe, Education, Experience
+from django.views.generic import TemplateView, ListView, DetailView
+from portfolio.models import AboutMe, Education, Experience, Project
 
 
 def index(request):
@@ -13,6 +14,45 @@ class AboutView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['about_me'] = AboutMe.objects.select_related('user').first()
-        context['experiences'] = Experience.objects.filter(about_me=context['about_me'])
-        context['education'] = Education.objects.filter(about_me=context['about_me'])
+        context['experiences'] = Experience.objects.filter(about_me=context['about_me']).order_by('-id')
+        context['educations'] = Education.objects.filter(about_me=context['about_me'])
         return context
+
+class CredentialsView(TemplateView):
+    template_name = 'credentials.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        about_me = AboutMe.objects.select_related('user').first()
+
+        context['about_me'] = about_me
+        context['experiences'] = Experience.objects.filter(about_me=about_me) if about_me else []
+        context['educations'] = Education.objects.filter(about_me=about_me) if about_me else []
+        context['social_media'] = about_me.social_media if about_me else {}
+        context['skills'] = about_me.skills.all() if about_me else []
+        return context
+
+
+class WorksView(ListView):
+    model = Project
+    template_name = 'works.html'
+    context_object_name = 'projects'
+
+    def get_queryset(self):
+        return Project.objects.prefetch_related('images').order_by('year')
+
+
+class WorkDetailView(DetailView):
+    model = Project
+    template_name = 'work_detail.html'
+    context_object_name = 'project'
+    
+    def get_queryset(self):
+        return Project.objects.prefetch_related('images').order_by('year')
+    
+    def get_object(self, queryset = None):
+        slug = self.kwargs.get('slug')
+        try:
+            return Project.objects.prefetch_related('images').get(slug=slug)
+        except Project.DoesNotExist:
+            raise Http404("Project does not exist")
